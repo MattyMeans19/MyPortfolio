5615defc1223a71b8f45bf99dd0d83bdc1a40ada
Matt <mattymeans@gmail.com>
2025-02-03 16:01:34 -0700

    Adding LyricQuery preview to Back-End

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 import pg from "pg";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://api.lyrics.ovh/v1/";
 
 app.use(bodyParser.urlencoded({extended: true }));
 
@@ -34,11 +36,29 @@ app.get("/rps", (req,res) =>{
     res.render("rps.ejs");
 });
 
+app.get("/lyriquery", (req,res) =>{
+    res.render("lyriquery.ejs");
+});
+
 
 app.post("/skillsModule", (req,res) =>{
     let selected = req.body.display;
     res.render("skills.ejs", {display: selected});
 });
 
+app.post("/search", async (req,res) => {
+    let searchArtist = req.body["artist"];
+    let searchSong = req.body["song"];
+    try{
+        const response = await axios.get(API_URL + searchArtist + "/" + searchSong);
+        const result = response.data;
+        var lineAdjust= result.lyrics.replace(/\n/g, "<br>");
+        res.render("lyriquery.ejs", {lyrics: lineAdjust, artist:searchArtist , song:searchSong});
+    } catch(error){
+        console.log("error")
+        res.render("lyriquery.ejs", {lyrics: "No Match Found, Try Again."});
+    }
+  });
 
-app.listen(process.env.PORT || 3000);
+
+app.listen(process.env.PORT || port);
